96d8eb6a271125863c9dc9d86586d7dc710dd2e7
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-08-10 06:11:44 +0900

    [feat] 바닐라 컴포넌트를 이용하여 기본적인 카운터 기능 구현
    - 기본적인 카운터( counter 보여주고, +1, -1) 동작 기능 구현

---
+++ counter-1/src/App.js
@@ -0,0 +1,38 @@
+import { $ } from "./utils/util.js";
+
+import Component from "./components/Component.js";
+
+export default class App extends Component {
+  constructor(...rest) {
+    super(...rest);
+    this.setState(0);
+  }
+
+  componentDidMount() {
+    const { handleIncrease, handleDecrease } = this;
+    $(".increaseBtn").addEventListener("click", handleIncrease.bind(this));
+    $(".decreaseBtn").addEventListener("click", handleDecrease.bind(this));
+  }
+  template() {
+    return `
+        <div class="container">
+        <h1>Counter</h1>
+        <!-- <form class="setDiffForm">
+        <input type="text" placeholder="1" />
+        <button type="submit">diff 설정</button>
+        </form> -->
+        <h2 class="counter">${this.state}</h2>
+        <button class="increaseBtn">+1</button>
+        <button class="decreaseBtn">-1</button>
+      </div>
+    `;
+  }
+
+  // custom handler
+  handleIncrease() {
+    this.setState(this.state + 1);
+  }
+  handleDecrease() {
+    this.setState(this.state - 1);
+  }
+}
