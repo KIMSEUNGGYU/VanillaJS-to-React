48c53d3dc1041bdbb9cea3ca6a3634a33f26d49c
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-08-11 06:32:24 +0900

    [refactor] temp data 폴더 이동

--- v1-todo-list-1/src/App.js
+++ v1-todo-list-1/src/App.js
@@ -1,24 +1,7 @@
 import { $ } from './utils/util.js';
+import { initialState } from './data/data.js';
 
 import Component from './components/Component.js';
-
-const initialState = [
-  {
-    id: 1,
-    text: '컴포넌트 개발',
-    done: true,
-  },
-  {
-    id: 2,
-    text: '리액트 흉내내기',
-    done: false,
-  },
-  {
-    id: 3,
-    text: 'TDD 해보기',
-    done: false,
-  },
-];
 
 const FILTER = Object.freeze({
   allView: '전체보기',
