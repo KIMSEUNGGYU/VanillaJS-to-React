5a262aacc22518cb25c13182cb1a61d58b2dea60
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2021-08-13 18:16:01 +0900

    [feat] users 에서 특정 user 클릭 시 해당 user 정보 보여주는 기능 구현

--- v1-async-test/src/App.js
+++ v1-async-test/src/App.js
@@ -14,37 +14,65 @@ export default class App extends Component {
     // API 요청 후 (초기) 상태를 설정할 때 async-await(Promise) 을 이용해야하는데
     // 기존에 사용했던 constructor 에서는 비동기 로직을 처리하기 힘듦
     // 그래서 init 함수를 만들어서 상태를 관리할 예정 (근데, 이 부분을 core/component 에 설정할 지 고민!)
-    const users = await userAPI.getUser();
+    const users = await userAPI.getUsers();
 
     this.setState({
       users,
+      user: null
     });
   }
 
   template() {
-    const { users } = this.state;
+    const { users, user } = this.state;
 
     return `
       <h1>Async Test</h1>  
-      <ul>
+      <ul class="user-list">
         ${users
-          .map((user) => `<li style="cursor: pointer"><b>${user.username}</b> (${user.name})</li>`)
+          .map((user) => `<li style="cursor: pointer" data-id="${user.id}">
+              <span> <b>${user.username}</b> (${user.name}) </span>
+            </li>`)
           .join('')}
       </ul>
-      <button>다시 불러오기</button>
+      <button class="re-fetch">다시 불러오기</button>
+      <hr />
+      ${ user ? 
+          `<section class="user">
+            <h1>${user.username}</h1>
+            <b>Email:</b> ${user.email}
+          </section>` : ""
+      }
     `;
   }
 
   componentDidMount() {
-    $('button').addEventListener('click', this.handleFeth.bind(this));
+    $('.re-fetch').addEventListener('click', this.handleFeth.bind(this));
+    $('.user-list').addEventListener('click', this.handleClickUser.bind(this))
   }
 
   // custom
   async handleFeth() {
     // update State?
-    const users = await userAPI.getUser();
+    const users = await userAPI.getUsers();
     this.setState({
+      ...this.state,
       users,
     });
+  }
+
+  async handleClickUser({ target }) {
+    if(target.tagName === "LI" || target.tagName === "UL") return;
+
+    const id = target.closest('li')?.dataset.id    
+    const user = await userAPI.getUser(id)
+
+    this.setState({
+      ...this.state,
+      user: {
+        id: user.id,
+        username: user.username,
+        email: user.email
+      }
+    })
   }
 }
--- v1-async-test/src/api/user.js
+++ v1-async-test/src/api/user.js
@@ -2,7 +2,7 @@ import { API } from '../utils/api.js';
 
 const USER_URL = `https://jsonplaceholder.typicode.com/users`;
 
-export const getUser = async () => {
+export const getUsers = async () => {
   try {
     const response = await API.get(USER_URL);
     if (response.ok) {
@@ -14,3 +14,16 @@ export const getUser = async () => {
     console.error(`Create Member Error: ${error}`);
   }
 };
+
+export const getUser = async (id) => {
+  try {
+    const response = await API.get(`${USER_URL}/${id}`);
+    if (response.ok) {
+      return response.json();
+    }
+
+    throw new Error(`${response.status}, ${response.statusText}`);
+  } catch (error) {
+    console.error(`Create Member Error: ${error}`);
+  }
+}
